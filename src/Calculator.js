7e7d21b846f5dc902071c6332dff16404be80ba7
dlgosla <dlgosla@gmail.com>
2021-06-30 16:21:35 +0900

    add css for calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,7 +8,7 @@ function Calculator() {
 
   const [outputs, setOutputs] = useState({
     current: '', 
-    previous: '',
+    previous: 'ans',
   });
 
 
@@ -38,43 +38,43 @@ function Calculator() {
         
       <div className="row-wrapper">
         <div className="name">
-          <span>React Calculator</span>
+          
         </div>
         <div className="display">
-          <div >{previous} =</div>
-          <div>{current}</div>
+          <div id="previous_value">{previous} =</div>
+          <div id="current_value">{current}</div>
         </div>
         <div className="row">
-          <button onClick={()=>setOutputs({...outputs, current:current+"9"} )}>9</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+"8"} )}>8</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+"7"} )}>7</button>
-          <button onClick={()=>setOutputs({...outputs, current:current.slice(0,-1)} )}>CE</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"9"} )}>9</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"8"} )}>8</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"7"} )}>7</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current.slice(0,-1)} )}>CE</button>
     
         </div>
         <div className="row">
-          <button onClick={()=>setOutputs({...outputs, current:current+"6"} )}>6</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+"5"} )}>5</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+"4"} )}>4</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+" + "} )}>+</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"6"} )}>6</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"5"} )}>5</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"4"} )}>4</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+" + "} )}>+</button>
         </div>
 
         <div className="row">
-          <button onClick={()=>setOutputs({...outputs, current:current+"3"} )}>3</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+"2"} )}>2</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+"1"} )}>1</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+" - "} )}>-</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"3"} )}>3</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"2"} )}>2</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"1"} )}>1</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+" - "} )}>-</button>
         </div>
 
         <div className="row">
-          <button onClick={()=>setOutputs({...outputs, current:current+"0"} )}>0</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+" / "} )}>/</button>
-          <button onClick={()=>setOutputs({...outputs, current:current+" * "} )}>*</button>
-          <button onClick={evaluation}> =</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+"0"} )}>0</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+" / "} )}>/</button>
+          <button className="calc_button" onClick={()=>setOutputs({...outputs, current:current+" * "} )}>*</button>
+          <button className="calc_button" onClick={evaluation}> =</button>
           
         </div>
 
-        <div className="History">
-          <h5>history</h5>
+        <div className="history">
+          <h3>HISTORY</h3>
           {historyList.map(info => <History sic={info.sic} result={info.result} />)}
         </div>
         
@@ -87,18 +87,16 @@ function Calculator() {
 
       //히스토리를 클릭 시 current 값을 클릭된 히스토리 값으로 변경 
       return (
-        <div className="row">
-        <button onClick={()=>setOutputs({...outputs, current:sic} )} >{sic}</button> 
-        <span> = </span>
-        <button onClick={()=>setOutputs({...outputs, current:result} )}>{result}</button>
+        <div className="history">
+          <button id="history_button"onClick={()=>setOutputs({...outputs, current:sic} )} >{sic}</button> 
+          <span id="equal"> = </span>
+          <button id ="history_button" onClick={()=>setOutputs({...outputs, current:result} )}>{result}</button>
         </div>
         
       )
   }
 
   }
-
-
 
 
 
